d0c99ad83bd20ee3f6f9643b4fd1fa5ace1e151d
Artag/Chris <scristxyz@gmail.com>
2024-12-03 11:57:32 -0500

    act

--- src/storage/storage.controller.ts
+++ src/storage/storage.controller.ts
@@ -4,10 +4,13 @@ import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class StorageController {
+
+  //TODO: hay que cambiarle el nombre por algo como event controller
   constructor(private readonly storageService: StorageService) {}
 
   @EventPattern('whatsapp.message')
   create(@Payload() message: any) {
+    console.log("mensaje en el storage");
     console.log(message);
     return "mensaje recivido de nats";
   }
