82f99840d9937121542934faf30b4692dda2c284
Artag/Chris <scristxyz@gmail.com>
2024-12-06 13:52:56 -0500

    act con prisma

--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -1,16 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { NewFilteredMessageDto } from './dtos';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class StorageService {
+export class StorageService extends PrismaClient implements OnModuleInit {
 
-    constructor() {}
+    constructor() {
+        super();
+    }
+    private readonly logger = new Logger('OrdersService');
+
+    async onModuleInit() {
+        await this.$connect();
+        this.logger.log('Database connected');
+      }
 
     ClassifyMessage( message: NewFilteredMessageDto){
         const {filteredMessage} = message as NewFilteredMessageDto
         const {whatsappMessage, to, name, agentType} = filteredMessage
         
-      
+      console.log(filteredMessage)
     }
 
 }
